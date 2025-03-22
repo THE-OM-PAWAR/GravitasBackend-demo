aeefc57c92a64381412ba8a0e1b4dee0e48a5c87
Gourav Rajak <gouravrajak+github@gmail.com>
2025-03-22 12:40:05 +0530

    update MongoDB connection string and add constants for database name

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
-
+const { DB_NAME } = require( "../utils/constants.js");
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+} catch (error) {
+    console.log("MONGODB connection FAILED ", error);
+    process.exit(1)
+}
 };
 
 module.exports = connectDB;
